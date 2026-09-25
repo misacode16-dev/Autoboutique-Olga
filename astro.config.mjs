3a38367d39b3fbcfe84a4c3e4c614958f9aed7fc
Misahel <misahelcastaneda@gmail.com>
2026-09-25 10:26:59 -0500

    correciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
    site: "https://autoboutiqueolga.com",
 
-   trailingSlash: "always",
+  trailingSlash: "always",  //ignore
 
 
    integrations: [
